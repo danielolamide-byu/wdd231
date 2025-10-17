8a4859d13fd3b9edd1a296e323fa7f7fe74706e0
danielolamide-byu <olamidedaniel874@gmail.com>
2025-10-17 05:02:23 +0100

    Final.

--- final_project/scripts/thankYou.js
+++ final_project/scripts/thankYou.js
@@ -4,12 +4,26 @@ const getString = window.location.search;
 const information = new URLSearchParams(getString);
 
 document.querySelector('#i').innerHTML = `
-<p>Name: ${ information.get('firstname') } ${ information.get('lastname') }</p>;
+<p>Name: ${information.get('firstname')} ${information.get('lastname')}</p>
 
-<p> ${ information.get('organizational-title') } of ${ information.get('organization-name') } </p>;
-<p>Email: ${ information.get('email') } </p>;
-<p> Phone: ${ information.get('phone') } </p>;
-<p> Membership Level: ${ information.get('product') } </p>;
-<p> Description: ${ information.get('description') }</p>;
-<p> Date: ${ information.get('dateOfLoad') }</p>;
-`
+<p> ${information.get('organizational-title')} of ${information.get('organization-name')} </p>
+<p>Email: ${information.get('email')} </p>
+<p> Phone: ${information.get('phone')} </p>
+<p> Membership Level: ${information.get('product')} </p>
+<p> Description: ${information.get('description')}</p>
+<p> Date: ${information.get('dateOfLoad')}</p>
+`;
+
+
+let numberOfVisits = document.getElementById("visit");
+
+let visits = Number(window.localStorage.getItem('store'));
+
+if (visits != 0) {
+    numberOfVisits.textContent = `Number Of Visits:${visits}`;
+}
+    else {
+            numberOfVisits.textContent = "First Visitor"
+        }
+visits ++ ;
+localStorage.setItem('store', visits);
